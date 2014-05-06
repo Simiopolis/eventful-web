b7002e4c0f02f11bd77e7e9720bb1a2ea91903f1
Si Wan Kim <siwan.kim@utexas.edu>
2014-05-06 00:08:35 -0500

    hook now listens to our prod repo

--- hook.js
+++ hook.js
@@ -3,7 +3,7 @@ var gith = require("gith").create(9001);
 var exec = require("child_process").exec;
 
 gith({
-    repo: 'Simiopolis/eventful-web'
+    repo: 'eventful-app/eventful-web'
     }).on('all', function(payload) {
 	console.log('received post!');
         if(payload.branch === 'master') {
